5704136d08f99714bdf533edbca3c1d6a8360c94
marxjoy <jooy@interia.pl>
2019-09-10 17:43:59 +0200

    '+

--- cart/cart.py
+++ cart/cart.py
@@ -2,3 +2,59 @@ from decimal import Decimal
 from django.conf import settings
 from menu.models import Meal
 
+class Cart:
+    
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+    
+    def add(self, meal, quantity=1, update_quantity=False):
+        meal_id = str(meal.id)
+        if meal_id not in self.cart:
+            self.cart[meal_id] = {'quantity': 0,
+                                  'price': str(meal.price)}
+        if update_quantity:
+            self.cart[meal_id]['quantity'] = quantity
+        else:
+            self.cart[meal_id]['quantity'] += quantity
+        self.save()
+            
+    def save(self):
+        self.session.modified = True
+        
+    def remove(self, meal):
+        meal_id = str(meal.id)
+        if meal_id in self.cart:
+            if self.cart[meal_id]['quantity'] == 1:
+                del self.cart[meal_id]
+            else:
+                self.cart[meal_id]['quantity'] -= 1
+            self.save()
+            
+    def __iter__(self):
+        meal_ids = self.cart.keys()
+        
+        meals = Meal.objects.filter(id__in=meal_ids)
+        
+        cart = self.cart.copy()
+        for meal in meals:
+            cart[str(meal.id)]['meal'] = meal
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+        
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+    
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+    
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+        
+    
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from menu.models import Meal
+from .cart import Cart
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def cart_add(request, meal_id):
+    cart = Cart(request)
+    meal = get_object_or_404(Meal, id=meal_id)
+    cart.add(meal)
+    return redirect('cart:cart_detail')
+
+@login_required
+def cart_remove(request, meal_id):
+    cart = Cart(request)
+    meal = get_object_or_404(Meal, id=meal_id)
+    cart.remove(meal)
+    return redirect('cart:cart_detail')
+
+@login_required
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 
+                  'cart/detail.html',
+                  {'cart': cart})

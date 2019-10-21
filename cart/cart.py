eebe559b9b73f3036b42a5c469da072e1e96c870
marxjoy <jooy@interia.pl>
2019-10-21 16:48:52 +0200

    Update cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -6,8 +6,10 @@ from .models import CartItem
 class Cart:
     def __init__(self, request):
         self.session = request.session
-        self.cart = self.session.get(settings.CART_SESSION_ID, {})
-        self.session[settings.CART_SESSION_ID] = self.cart
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
 
 
     def add(self, cart_item_id):
